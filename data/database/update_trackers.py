b6e1add427d862a8b45216fece36f34954758ec0
gustavoamarante <gusamarante@gmail.com>
2023-04-23 17:29:50 -0300

    finished readers and uploading BR credit to the database

--- data/database/update_trackers.py
+++ data/database/update_trackers.py
@@ -1,6 +1,10 @@
-from data import read_etf, tracker_uploader, tracker_feeder
+from data import read_etf, read_ida, tracker_uploader
 
 # BR Equities
 bre_names = ['BOVA', 'BCIC', 'BDEF', 'BBSD', 'BREW', 'BRAX', 'BMMT', 'SMAL']
 bre_data = read_etf(bre_names)
 tracker_uploader(bre_data, 'br equities')
+
+# BR Credit
+brc_data = read_ida()
+tracker_uploader(brc_data, 'br credit')
