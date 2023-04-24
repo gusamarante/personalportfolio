ba1bac8c43902e8f9a3754237ddabefe0538f1c7
gamarante <gusamarante@gmail.com>
2023-04-24 10:23:51 -0200

    Progress on reading the current portfolio

--- data/database/update_composition.py
+++ data/database/update_composition.py
@@ -1,3 +1,12 @@
 # TODO Read the current portfolio composition from excel and save it to the database
 # TODO Save the current portfolio weights (actual weights), current porfolio values (actual values)
 
+import pandas as pd
+from data import DROPBOX
+
+cols = [5, 6, 7, 8, 9]
+df = pd.read_excel(DROPBOX.joinpath('Personal Portfolio.xlsx'),
+                   usecols=cols)
+df = df.dropna()
+
+print(df)
