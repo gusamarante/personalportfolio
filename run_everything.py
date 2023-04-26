97effd39268fcc0fe33920eee806e293eccd0316
gustavoamarante <gusamarante@gmail.com>
2023-04-25 23:36:15 -0300

    progress

--- run_everything.py
+++ run_everything.py
@@ -1,4 +1,4 @@
-
-# Creates the database
 from data.database import create_database
 from data.database import update_trackers
+from data.database import update_focus
+from br_nominal_rates import value_focus_survey
